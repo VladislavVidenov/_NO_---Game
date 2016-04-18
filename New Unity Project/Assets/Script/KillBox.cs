56fb2369ddc9af60ccae7329b89c518e770754dd
jeremyclarksonandthefortythieves <kranendonk.willem@googlemail.com>
2016-04-18 16:14:57 +0200

    Updated Stuff
    Broken Loading, Fading... EVERYTHING

--- New Unity Project/Assets/Script/KillBox.cs
+++ New Unity Project/Assets/Script/KillBox.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class KillBox : MonoBehaviour {
 
     public GameObject spawnPoint;
+	public GameObject ballSpawn;
 
     void OnTriggerEnter(Collider other) {
-        if (other.name == "Player") {
-            other.transform.position = spawnPoint.transform.position;
-        }
+		if (other.name == "Player") {
+			other.transform.position = spawnPoint.transform.position;
+		} else if (other.name == "pickupBall") {
+			other.transform.position = ballSpawn.transform.position;
+		}
     }
 }
--- New Unity Project/Assets/Script/TriggerSocket.cs
+++ New Unity Project/Assets/Script/TriggerSocket.cs
@@ -3,15 +3,11 @@ using System.Collections;
 
 public class TriggerSocket : MonoBehaviour {
 
-    public Animator socketAnimator;
-
-    void Start() {
-        socketAnimator = GetComponent<Animator>();
-    }
-
     void OnTriggerEnter(Collider other) {
-        if (other.name == "RollerBall") {
-            socketAnimator.SetInteger("craneMove", 1); 
+		Animator socketAnimator = GameObject.Find ("crane").GetComponent<Animator> ();
+		if (other.name == "pickupBall") {
+			Debug.Log ("ball in place");
+			socketAnimator.SetInteger("craneMove", 1); 
         }
     }
 }
